9d17bd08499c3f159e513ed3d392073e37d50fe6
Lokesh Karanam <sunnylokesh3@gmail.com>
2025-07-06 21:40:27 -0500

    Update config.py for Azure PostgreSQL best practices and add DATABASE_URL support

--- config.py
+++ config.py
@@ -5,12 +5,16 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, ".env"))
 
+DATABASE_URL = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('AZURE_POSTGRESQL_USER'),
+    dbpass=os.getenv('AZURE_POSTGRESQL_PASSWORD'),
+    dbhost=os.getenv('AZURE_POSTGRESQL_HOST'),
+    dbname=os.getenv('AZURE_POSTGRESQL_NAME')
+)
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "sqlite:///" + os.path.join(
         os.path.abspath(os.path.dirname(__file__)), "app.db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
